e2f2aec2e4f267020a99d175f2113645d0a34f4b
moohao <mh@lindo.io>
2017-12-08 13:51:30 +0800

    上传图片 图片名随机

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -59,8 +59,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    if super.present? && original_filename
+      @name ||= Digest::MD5.hexdigest(File.dirname(current_path)).slice(0, 12)
+      "#{@name}.#{file.extension}".downcase
+    end
+  end
 
 end
